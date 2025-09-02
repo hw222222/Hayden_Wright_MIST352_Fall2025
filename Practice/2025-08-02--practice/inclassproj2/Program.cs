f304b8cc6654181e90b439ed5fae063a445f9a22
hayden <hw00050@mix.wvu.edu>
2025-09-02 18:38:26 -0400

    corrected an incorrect filename for Task1, and updating to include 9/2's in class work

---
+++ Practice/2025-08-02--practice/inclassproj2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+namespace Practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // defined 2 variables
+            double dblVal1, dblVal2;
+
+            // ask user for input 1
+            Console.WriteLine("Give me value 1");
+            dblVal1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(dblVal1);
+
+            // ask user for input 1
+            Console.WriteLine("Give me value 2");
+            dblVal2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(dblVal2);
+
+            Console.WriteLine(dblVal1 + dblVal2);
+            Console.WriteLine($"The Sum of {dblVal1} and {dblVal2} is {dblVal1 + dblVal2}");
+        }
+    }
+}
---
+++ Practice/Practice3_Grades/Practice3Grades/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string strName = " ";
+        Console.WriteLine("Give me your name: ");
+        strName = Console.ReadLine();
+        double dblGrade1, dblGrade2, dblGrade3, dblGPA;
+        // Console.WriteLine(strName);
+
+        // Accept Grade 1
+        Console.WriteLine("What is grade 1? ");
+        dblGrade1 = Double.Parse(Console.ReadLine());
+
+        // Accept Grade 2
+        Console.WriteLine("What is grade 2? ");
+        dblGrade2 = Double.Parse(Console.ReadLine());
+
+        //accept Grade 3
+        Console.WriteLine("What is grade 3? ");
+        dblGrade3 = Double.Parse(Console.ReadLine());
+
+        dblGPA = (dblGrade1 + dblGrade2 + dblGrade3) / 3;
+
+        Console.WriteLine($"Hello {strName}, your grades {dblGrade1}, {dblGrade2}, and {dblGrade3} equal a gpa of {dblGPA}");
+
+        bool blnFail = dblGPA<50;
+        Console.WriteLine($"failed? {blnFail}");
+
+    }
+}
